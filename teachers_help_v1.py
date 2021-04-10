3977409c224e4aefaa643a42bd96641e7a66645d
Karolina D <karolina.dike@gmail.com>
2021-04-10 15:40:54 +0200

    Final versions

--- teachers_help_v1.py
+++ teachers_help_v1.py
@@ -12,6 +12,22 @@ def csv_reader(file):
         #for row in csv_file_reader:
                 #print(f'{", ".join ( row )}')
 
+def open_file(file):
+    with open(file,  encoding="utf-8") as file_opened:
+        lines = file_opened.readlines()
+        print(lines)
+        return lines
+
+
+def data_imported(data):
+
+    dict_data = {"names": data[1],
+                 "surnames": data[1],
+                 "tasks_missing" : data[2],
+                 "grade": data[3]}
+    print(dict_data)
+    return dict_data
+
 
 if __name__ == '__main__':
         try:
@@ -19,6 +35,9 @@ if __name__ == '__main__':
 
         except FileNotFoundError as err:
             print(err, "File not found. Check the spelling.")
+
+message = str(open_file("message.txt"))
+stu_dict = data_imported(students_read)
 
 
 
